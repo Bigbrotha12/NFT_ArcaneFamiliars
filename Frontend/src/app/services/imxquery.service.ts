c68c7685be60d159dc1a961043de68b674313487
Rafael Mendoza <rafael.mendoza64@gmail.com>
2022-08-23 20:11:03 +0200

    configure app for webpack >5 polyfill

--- Frontend/src/app/services/imxquery.service.ts
+++ Frontend/src/app/services/imxquery.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Link } from '@imtbl/imx-sdk';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IMXqueryService {
 
-  constructor() { }
+// Mainnet
+//const linkAddress = 'https://link.x.immutable.com';
+//const apiAddress = 'https://api.x.immutable.com/v1';
+
+// Ropsten Testnet
+linkAddress = 'https://link.ropsten.x.immutable.com';
+//const apiAddress = 'https://api.ropsten.x.immutable.com/v1';
+
+// Link SDK
+link = new Link(this.linkAddress);
+
+constructor() { }
 }
