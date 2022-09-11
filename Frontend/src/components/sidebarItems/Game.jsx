a41b39b7f15021f2e1d9d8d7e5f7a45375b686f4
Rafael Mendoza <rafael.mendoza64@gmail.com>
2022-09-11 19:54:08 +0200

    change game canvas

--- Frontend/src/components/sidebarItems/Game.jsx
+++ Frontend/src/components/sidebarItems/Game.jsx
@@ -3,10 +3,9 @@ import React from 'react';
 import styles from '../../styles/lightStyles.module.css';
 import Unity from "../../API/Unity.jsx";
 
+
 export default function Game() {
   return (
-      <div className={styles.gameCanvas}>
-        <Unity />
-      </div>
+    <Unity />
   )
 }
