d7cad776ba40dbb1bc0b2257519c679e68b1f6cb
Rafael Mendoza <68668067+Bigbrotha12@users.noreply.github.com>
2022-09-29 21:48:54 +0200

    Update server_stack.tf

--- backend/terraform/server_stack.tf
+++ backend/terraform/server_stack.tf
@@ -1,1 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
 
+provider "aws" {
+  profile = "default"
+  region = "us-east-1"
+}
+
+resource "aws_instance" "AFWebServer" {
+  ami           = "ami-013066dc1f1a0259b"
+  instance_type = "t2.micro"
+  tags = {
+    Name        = "AFWebServer"
+    Environment = "Dev"
+  }
+}
