6fd6ca940207ed21f42632a2130c7b728a09a0ad
Rafael Mendoza <rafael.mendoza64@gmail.com>
2022-10-05 09:25:06 +0200

    create config file for constant values

---
+++ backend/server/arcane.config.js
@@ -0,0 +1,14 @@
+export const Config = {
+    app_version: "1.0.0",
+    chain_version: "1.0.0",
+    game_version: "1.0.0",
+    app_name: "Arcane Familiars",
+    game_codehash: "",
+    tiers: [5, 11, 15, 17],
+    IMX_API: {
+        baseURL: "https://api.x.immutable.com",
+        connection_timeout: 3000,
+        retries: 3,
+        retry_interval: 200
+    }
+}
