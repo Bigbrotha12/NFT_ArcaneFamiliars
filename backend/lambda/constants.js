eb4e646e4a953cb9e59b7eec017f407752bf32c0
Rafael Mendoza <rafael.mendoza64@gmail.com>
2022-10-13 23:12:05 +0200

    rename file and remove outdated reference material

--- backend/lambda/constants.js
+++
@@ -1,9 +0,0 @@
-const constants = {
-    UNITY_CODEHASH: "",
-    TIER_1: 5,
-    TIER_2: 11,
-    TIER_3: 15,
-    TIER_4: 17
-}
-
-module.exports = constants;
--- backend/lambda/mongoClient.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-
-// MongoDB connection
-let client = new MongoClient(process.env.MONGODB_URI,
-  { useNewUrlParser: true, useUnifiedTopology: true, wtimeoutMS: 2000 });
-module.exports = client.connect(); 
