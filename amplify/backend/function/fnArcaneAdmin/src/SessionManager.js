a52e0d723a9a16ca0cc8c14b0a1ad19febf995bd
Rafael Mendoza <rafael.mendoza64@gmail.com>
2022-10-18 19:12:56 +0200

    move validation data to header

--- amplify/backend/function/fnArcaneAdmin/src/SessionManager.js
+++ amplify/backend/function/fnArcaneAdmin/src/SessionManager.js
@@ -3,14 +3,14 @@ module.exports = class SessionManager {
     static async checkSession(conn, event) {
         let [client, db] = conn;
         let result = await db.collection("session").find(
-            {address: event.body.eth_address}, {login_timestamp: 0}).toArray();
-        return result[0]
+            {address: event.headers.eth_address}, {login_timestamp: 0}).toArray();
+        return {statusCode: 200, body: result[0]}
     }
 
     static async isRegistered(conn, event) {
         let [client, db] = conn;
         let result = await db.collection("users").find(
-            {_id: event.body.eth_address}).toArray();
+            {_id: event.headers.eth_address}).toArray();
         return result.length > 0;
     }
 
@@ -18,7 +18,7 @@ module.exports = class SessionManager {
         if(!this.isRegistered(conn, event)) {
             let [client, db] = conn;
             let result = await db.collection("users").insertOne({
-                _id: event.body.eth_address,
+                _id: event.headers.eth_address,
                 total_minted: 0,
                 register_timestamp: `${Math.floor(Date.now()/1000)}`,
                 last_active_timestamp: `${Math.floor(Date.now()/1000)}`,
@@ -39,25 +39,23 @@ module.exports = class SessionManager {
         let [client, db] = conn;
         let now = Math.floor(Date.now()/1000);
         let result = await db.collection("session").updateOne(
-            {address: event.body.eth_address}, 
+            {address: event.headers.eth_address}, 
             {$set: {
-            address: event.body.eth_address,
-            session_id: event.body.eth_signature,
-            login_timestamp: `${now}`,
+            address: event.headers.eth_address,
+            session_id: event.headers.eth_signature,
+            login_timestamp: event.headers.eth_timestamp,
             expiration: `${now + (5 * 60 * 60)}`,         // set initial expiration for 5 hours
-            max_expiration: `${now + (24 * 60 * 60)}`}    // request user to re-login after 24 hours  
+            max_expiration: `${now + (24 * 60 * 60)}`}   // request user to re-login after 24 hours  
         }, {upsert: true});
-        return {statusCode: 200, result: {...result, login_timestamp: `${now}`}}
+        return {statusCode: 201, body: "Login Successful" }
     }
 
     // if user saves game or update game progress, session is extended up until max expiration
     static async refreshSession (conn, event, session) {
         let [client, db] = conn;
         let now = Math.floor(Date.now()/1000);
-        if(!session.isActive) 
-        {return {statusCode: 401, message: "request authentication"}}
         let result = await db.collection("session").updateOne(
-            {address: event.body.eth_address},
+            {address: event.headers.eth_address},
             {$set: {
                 expiration: `${Math.min(now + 5 * 60 * 60, session.max_expiration)}` //extend session for 5 hours
         }});
@@ -65,28 +63,28 @@ module.exports = class SessionManager {
 
     static async logoutUser(conn, event) {
         let [client, db] = conn;
-        let result = await db.collection("session").deleteOne({address: event.body.eth_address});
-        return {statusCode: 200, result: result}
+        let result = await db.collection("session").deleteOne({address: event.headers.eth_address});
+        return {statusCode: 200, body: "Logout Successful"}
     }
 
     static async loadGame(conn, event, session) {
         let [client, db] = conn;
         if(!session.isActive) 
-        {return {statusCode: 401, message: "request authentication"}}
-        let result = await db.collection("users").find({_id: event.body.eth_address}, 
+        {return {statusCode: 401, body: "invalid authentication"}}
+        let result = await db.collection("users").find({_id: event.headers.eth_address}, 
             {saveData: 1, _id: 0}).toArray();
-        return {statusCode: 200, data: result[0]};
+        return {statusCode: 200, body: result[0]};
     }
 
     static async saveGame(conn, event, session) {
         if(!session.isActive && !session.isGameValid) 
-        {return {statusCode: 401, message: "request authentication"}}
+        {return {statusCode: 401, body: "invalid authentication"}}
 
         let [client, db] = conn;
         let result;
         if(event.body.saveData.progress) {
             result = await db.collection("users").updateOne(
-                {_id: event.body.eth_address,
+                {_id: event.headers.eth_address,
                 "saveData.progress": "0"},
                 {$set: {
                     "saveData.items": event.body.saveData.items,
@@ -96,7 +94,7 @@ module.exports = class SessionManager {
                 }});
         } else {
             result = await db.collection("users").updateOne(
-                {_id: event.body.eth_address},
+                {_id: event.headers.eth_address},
                 {$set: {
                     "saveData.items": event.body.saveData.items,
                     "saveData.location": event.body.saveData.location,
@@ -104,6 +102,6 @@ module.exports = class SessionManager {
                 }});
         }
         await this.refreshSession(conn, event, session);
-        return {statusCode: 200, result: result}
+        return {statusCode: 200, body: "Game save successful"}
     }
 }
