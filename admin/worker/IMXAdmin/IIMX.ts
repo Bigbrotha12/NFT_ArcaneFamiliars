a5425133a5b5c1ba4447ec9993a7f334114d0441
Rafael Mendoza <rafael.mendoza64@gmail.com>
2022-11-03 01:44:47 +0100

    add imx admin functions

---
+++ admin/worker/IMXAdmin/IIMX.ts
@@ -0,0 +1,14 @@
+
+export default interface IIMX {
+    // isRegistered(): any,
+    // registerAccount(): any,
+    createProject(): any,
+    getProjectsData(): any
+    createCollection(id: number): any,
+    addMetadataSchema(): any
+    getCollectionDetails(): any,
+    getAllCollections(): any
+    updateCollection(): any
+    getMetadataSchema(): any
+    updateMetadataById(id: Array<string>): any
+}
---
+++ admin/worker/IMXAdmin/IMXController.ts
@@ -0,0 +1,47 @@
+
+export default class IMXController {
+
+    constructor() {
+
+    }
+
+    async registerAccount() {
+
+    }
+
+    async createProject() {
+
+    }
+
+    async getProjectData() {
+
+    }
+
+    async createCollection() {
+        
+    }
+
+    async addMetadataSchema() {
+        
+    }
+
+    async getCollectionDetails() {
+
+    }
+
+    async getAllCollections() {
+
+    }
+
+    async updateCollection() {
+
+    }
+
+    async getMetadataSchema() {
+
+    }
+
+    async updateMetadataByName() {
+
+    }
+}
