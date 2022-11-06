2ba7ee8c9332c8fa1d54f709377f0e849fa50a91
Rafael Mendoza <rafael.mendoza64@gmail.com>
2022-11-06 23:35:33 +0100

    reorg

--- frontend/src/components/03_Body/Minter/Other.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ComingSoon from "../../Common/ComingSoon";
-
-export default function Other() {
-  return (
-    <ComingSoon />
-  )
-}
--- frontend/src/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { createRoot, Root } from 'react-dom/client';
-import "./styles.css";
-import App from "./App";
-
-const container: HTMLElement = document.getElementById("root")!
-const root: Root = createRoot(container);
-
-root.render(<App />);
