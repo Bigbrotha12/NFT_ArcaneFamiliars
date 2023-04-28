9bce981f7270003f18f43b8e39e8e0875944df4c
Bigbrotha12 <rafael.mendoza64@gmail.com>
2023-04-28 19:51:16 +0200

    add unity api

--- frontend/src/components/03_Body/Game/UnityCanvas.tsx
+++ frontend/src/components/03_Body/Game/UnityCanvas.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Config from "../../../app/constants/AppConfig";
 import { Authentication } from "../../../types/IMX";
 import { Unity, useUnityContext } from "react-unity-webgl";
@@ -14,6 +14,7 @@ export default function UnityCanvas() {
   const [client, auth,,] = React.useContext<IMXHandler>(IMX);
 
   const [gameLaunch, setGameLaunch] = React.useState(false);
+  const [showGameMenu, setShowGameMenu] = React.useState(false);
   const { unityProvider, sendMessage, addEventListener, removeEventListener, unload, loadingProgression } =
     useUnityContext({
       loaderUrl: Config.Unity.loader,
@@ -31,26 +32,56 @@ export default function UnityCanvas() {
     if(!userAddress) {
       await client.connect();
     }
-    await client.authenticate();
   
-    if(auth) {
-      sendMessage("APIHandler", "ReceiveAuthentication", JSON.stringify(auth));
+    if(userAddress) {
+      sendMessage("GameManager", "ReceiveWeb3Address", JSON.stringify(userAddress));
     } 
-  }, [userAddress, auth]);
+  }, []);
+
+  const ShowMenu = React.useCallback(async () => {  
+    setShowGameMenu(true);
+  }, []);
+
+  const displayLevelInfo = () => {
+    sendMessage("GameManager", "ReceiveLevelInfo");
+  }
+
+  const displayLevelSelect = () => {
+    sendMessage("GameManager", "ReceiveLevelSelect");
+  }
+
+  const restartLevel = () => {
+    sendMessage("GameManager", "ReceiveRestartLevel");
+  }
+
+  const HideMenu = React.useCallback(async () => {  
+    setShowGameMenu(false);
+  }, []);
 
   React.useEffect(() => {
-    addEventListener("RequestAuth", requestAuthentication);
+    addEventListener("RequestAddress", requestAuthentication);
+    addEventListener("DisplayMenu", ShowMenu);
+    addEventListener("HideMenu", HideMenu);
     return () => {
-      removeEventListener("RequestAuth", requestAuthentication);
+      removeEventListener("RequestAddress", requestAuthentication);
+      removeEventListener("DisplayMenu", requestAuthentication);
+      removeEventListener("HideMenu", requestAuthentication);
     };
-  }, [addEventListener, removeEventListener, requestAuthentication]);
+  }, [requestAuthentication, ShowMenu, HideMenu]);
   
   return (   
       <div className="relative bg-black w-full h-full flex align-middle justify-center">
         {!gameLaunch && <Material.Button sx={{position: "absolute", top: "40%", height: "auto", margin: "auto"}} variant="contained" onClick={() => setGameLaunch(true)} startIcon={<Material.PlayArrow/>}>Launch Game</Material.Button>}
         {gameLaunch && loadingProgression != 1 && <Material.CircularProgress sx={{position: "absolute", top: "40%", margin: "auto"}} variant="determinate" value={loadingProgression * 100} />}
         {gameLaunch && <Unity className="w-[900px] h-[500px]" unityProvider={unityProvider} />}
-        {gameLaunch && <Material.Button sx={{position: "absolute", height: "auto", margin: "auto", bottom: "-94px"}} variant="contained" onClick={handleClose} startIcon={<Material.Close/>}>Close Game</Material.Button>}  
+        {gameLaunch && showGameMenu &&
+        <div className="absolute flex gap-3 justify-center bottom-[-20%]">
+          <Material.Button variant="contained" onClick={displayLevelInfo} startIcon={<Material.MenuIcon/>}>Level Details</Material.Button>
+          <Material.Button variant="contained" onClick={displayLevelSelect} startIcon={<Material.SearchIcon/>}>Level Select</Material.Button>
+          <Material.Button variant="contained" onClick={restartLevel} startIcon={<Material.PlayArrow/>}>Restart Level</Material.Button>
+        </div>
+          }
+        {gameLaunch && <Material.Button sx={{position: "absolute", height: "auto", margin: "auto", bottom: "-180px"}} variant="contained" onClick={handleClose} startIcon={<Material.Close/>}>Close Game</Material.Button>}  
       </div>
   );
 }
